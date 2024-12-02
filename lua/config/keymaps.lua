09f7b727d895df2471223e1826535a98922ca6e9
Uzi Refaeli <uzi.refaeli@healthee.co>
2024-12-02 08:46:17 +0200

    adding leader-e for lsp error floating dialog

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -8,3 +8,5 @@ keymap.set('n', '<leader>tt', '<Cmd>NvimTreeOpen<CR>', {})
 -- Example: Map `<leader>/` to toggle comments
 vim.api.nvim_set_keymap('n', '<leader>/', ':CommentToggle<CR>', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('v', '<leader>/', ':CommentToggle<CR>', { noremap = true, silent = true })
+-- Open error
+vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { noremap = true, silent = true })
