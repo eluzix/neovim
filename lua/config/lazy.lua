5984742af4ac1566b46cfd204fc97a4914f8307b
eluzix <eluzix@gmail.com>
2025-06-12 10:13:13 +0300

    adding hunk diff editor

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -58,6 +58,13 @@ require("lazy").setup({
         { "jeetsukumaran/vim-pythonsense", ft = { "python" } },
         
         { "rmehri01/onenord.nvim" },
+        {
+          "julienvincent/hunk.nvim",
+          cmd = { "DiffEditor" },
+          config = function()
+            require("hunk").setup()
+          end,
+        },
     },
     -- Configure any other settings here. See the documentation for more details.
     -- colorscheme that will be used when installing plugins.
